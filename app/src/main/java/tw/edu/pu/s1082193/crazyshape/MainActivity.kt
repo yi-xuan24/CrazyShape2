863576afa077e5137c4e92309d6a3366fb5f9ddc
carzy <ss30474849@gmail.com>
2021-05-20 22:19:29 +0800

    2021上機考V0_(陳怡璇)

---
+++ app/src/main/java/tw/edu/pu/s1082193/crazyshape/MainActivity.kt
@@ -0,0 +1,28 @@
+package tw.edu.pu.s1082193.crazyshape
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Toast.makeText(baseContext, "作者：陳怡璇", Toast.LENGTH_LONG).show()
+
+        imgNext.setOnLongClickListener(object : View.OnLongClickListener {
+            override fun onLongClick(p0: View?): Boolean {
+                intent = Intent(this@MainActivity, GameActivity::class.java)
+                startActivity(intent)
+                return true
+            }
+        })
+    }
+}
+
+
+
